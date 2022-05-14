27920b8a806178ee0fd633c5ea5a4e1f3d349063
hem1t <sunny10fb@gmail.com>
2022-05-14 11:23:28 +0530

    Working notification and some GUI

---
+++ background.js
@@ -0,0 +1,73 @@
+function article_to_html(article) {
+}
+
+async function load_topics() {
+    // topics example
+    let topics = [
+        ["hello", "working"],
+    ];
+
+    //let res = await fetch("https://iq.opengenus.org/");
+    console.log("working...");
+    //res = await res.text();
+    return topics;
+}
+
+chrome.runtime.onInstalled.addListener( function () {
+    // set settings
+    chrome.storage.sync.set(
+        {
+            "settings": {
+                "theme" : "light",
+                "refresh_time" : 1,
+            }
+        }
+    );
+    chrome.storage.local.set({
+        "last": Date.now()
+    });
+    console.log("Setting default settings");
+
+    // topics
+    load_topics().then(topics => {
+        chrome.storage.local.set( {
+            "topics": topics,
+        });
+    });
+
+    // Initial alarm
+    chrome.alarms.create("Initial Alarm", {
+        "periodInMinutes": 1440
+    });
+});
+
+// When alarm goes off Notify user to use IQNotify.
+chrome.alarms.onAlarm.addListener( function (alarm) {
+    chrome.storage.sync.get(["last"], (settings) => {
+        let day = parseInt((Date.now() - settings.last) / 86400000);
+        let s = "";
+        if (day > 1) {
+            s = "'s";
+        }
+        chrome.notifications.create({
+            "type": "basic",
+            "title": "IQNotify reminder",
+            "message": "It's been " + day + " day" + s + " seens you opened up IQNotify.",
+            "iconUrl": "./icon32x32.png",
+        });
+    });
+    console.log(alarm);
+});
+
+// Extra
+chrome.runtime.onStartup.addListener( function () {
+    // when browser starts up
+});
+// Extra
+chrome.runtime.onSuspend.addListener( () => {
+    chrome.action.setBadgeText({"text": ""});
+});
+
+
+// Extra
+chrome.notifications.onClicked.addListener( () => {});
---
+++ popup.js
@@ -0,0 +1,78 @@
+// Declared codes, (code that only runs when called)
+
+const hours_day = 24;
+const millis_hour = 3600000;
+const minutes_hour = 60;
+const millis_minute = 60000;
+const settings_src = "./gear.png";
+const topic_src = "./back.png";
+const topics = document.getElementById("tab-topics");
+const settings = document.getElementById("tab-settings");
+const change_btn = document.getElementById("btn");
+const btn_img = document.getElementById("btn-img");
+let hidden = "tab";
+//
+change_btn.addEventListener("click", () => {
+    if (hidden === "settings") {
+        hidden = "topics";
+        // button
+        btn_img.alt = "Settings";
+        btn_img.src = settings_src;
+        // tabs
+        settings.classList = ["tab", " hidden"];
+        topics.classList = ["tab"];
+    } else {
+        hidden = "settings";
+        // button
+        btn_img.alt = "Topics";
+        btn_img.src = topic_src;
+        // tabs
+        settings.classList = ["tab"];
+        topics.classList = ["tab", " hidden"];
+    }
+});
+
+// function to write topics on popup.html
+function append_topic(topic) {
+    let card = "";
+    for (let a in topic) {
+        card = card + "<br>" + topic[a];
+    }
+    topics.innerHTML = topics.innerHTML
+        + card;
+}
+
+// ENDING of declared codes.
+
+// Since this runs everytime user opens the popup
+// So, noting the time.
+chrome.storage.local.set({
+    "last": Date.now()
+});
+
+// Setting Alarm
+// Destroy first if there is any.
+
+chrome.alarms.clearAll((done) => {
+    if (!done) {
+        console.log();
+    }
+});
+chrome.storage.sync.get(["settings"], (settings) => {
+    let time = settings.settings.refresh_time * minutes_hour * hours_day;
+    console.log(time);
+    chrome.alarms.create({
+        "periodInMinutes": time
+    });
+});
+
+
+// chrome.action.setBadgeText({text: "1"});
+// chrome.notifications.getPermissionLevel(level => {console.log(level);});
+
+chrome.storage.local.get(["topics"], (cards) => {
+    cards = cards.topics;
+    for (let i in cards) {
+        append_topic(cards[i]);
+    }
+});
