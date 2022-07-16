7c1fe44357332a5037d08a7d2eb5ddaa1e4af3bb
hem1t <sunny10fb@gmail.com>
2022-07-16 22:29:18 +0530

    global progress bar

--- popup.js
+++ popup.js
@@ -24,7 +24,43 @@ chrome.alarms.clearAll();
 
 // CHECKLIST requires lots of ELs
 // and localStorage
+const checklist_count = 100;
+let container_titles = document.getElementsByClassName("container-title");
+let checklist_checkbox = document.getElementsByClassName("plus-minus-checkbox");
+let checklist_topic_descs = document.getElementsByClassName("topic-desc");
+let checklist_topic_title = document.getElementsByClassName("topic-title");
+let checklist_topic_head = document.getElementsByClassName("topic-head");
+let topic_checkbox = document.querySelectorAll(".topic-head input");
+for (let i = 0; i < checklist_count; i++) {
+    // EL to animate .topic-info
+    checklist_topic_title[i].addEventListener('click', function() {
+        checklist_topic_descs[i].classList.toggle("topic-hide");
+        checklist_checkbox[i].checked = !(checklist_checkbox[i].checked);
+    });
+    checklist_checkbox[i].addEventListener('change', function() {
+        checklist_topic_descs[i].classList.toggle('topic-hide');
+    });
+}
 
+
+// Showing progress.
+// functions to draw
+function global_progress_bar(num) {
+    // updating number
+    document.getElementById("checklist-status-number").innerHTML = num + "/" + checklist_count;
+    // updating the bar
+    document.getElementById("global-progress-bar").style["width"] = num + "%";
+}
+
+// function container_progress_in_number(index, num) {
+// }
+
+// function update_progress() {
+
+// }
+
+
+// array and set conversion
 function to_array(set) {
     let array = [];
     set.forEach((elem) => {
@@ -41,29 +77,31 @@ function to_set(arr) {
     return set;
 }
 
-let checklist_checkbox = document.getElementsByClassName("plus-minus-checkbox");
-let checklist_topic_descs = document.getElementsByClassName("topic-desc");
-let checklist_topic_title = document.getElementsByClassName("topic-title");
-let checklist_topic_head = document.getElementsByClassName("topic-head");
-let topic_checkbox = document.querySelectorAll(".topic-head input");
-for (let i = 0; i < 100; i++) {
-    // EL to animate .topic-info
-    checklist_topic_title[i].addEventListener('click', function() {
-        checklist_topic_descs[i].classList.toggle("topic-hide");
-        checklist_checkbox[i].checked = !(checklist_checkbox[i].checked);
-    });
-    checklist_checkbox[i].addEventListener('change', function() {
-        checklist_topic_descs[i].classList.toggle('topic-hide');
-    });
-}
-
 chrome.storage.sync.get(["checklist"])
     .then((cl) => {
         let checked_checklist = (cl.checklist === undefined) ?
             new Set() :
             to_set(JSON.parse(cl.checklist));
+        // Intial progress bar.
+        global_progress_bar(checked_checklist.size);
+        // EL for reset button
+        document.getElementById("checklist-status-reset").addEventListener("click", () => {
+            // update visual
+            global_progress_bar(0);
+            checked_checklist.forEach((checkbox_id) => {
+                let checkbox = document.getElementById(checkbox_id);
+                checkbox.checked = false;
+                checkbox.parentElement.getElementsByClassName("topic-title")[0].style['text-decoration'] = 'none';
+            });
+            // update storage
+            checked_checklist = new Set();
+            localStorage.setItem('checklist', '[]');
+            chrome.storage.sync.set({
+                "checklist": '[]'
+            });
+        });
 
-        for (let i = 0; i < 100; i++) {
+        for (let i = 0; i < checklist_count; i++) {
             // EL to add (if done or not) to storage sync.
             topic_checkbox[i].addEventListener('change', function() {
                 if (this.checked) {
@@ -81,6 +119,7 @@ chrome.storage.sync.get(["checklist"])
                 chrome.storage.sync.set({
                     "checklist": stringified
                 });
+                global_progress_bar(checked_checklist.size);
             });
         }
 
@@ -92,6 +131,7 @@ chrome.storage.sync.get(["checklist"])
         });
 
     });
+
 
 
 // Loading theme after all above pre-loading
